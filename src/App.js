9d49c2ec9ed69d960a55959601e0cd0e37625bc3
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-11 00:33:20 +0600

    Added animated custom cursor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import GetInTouch from './Components/GetInTouch/GetInTouch';
-import Header from './Components/Header/Header';
-import Navbar from './Components/Navbar/Navbar';
-import Projects from './Components/Projects/Projects';
-import Skills from './Components/Skills/Skills';
+import "./App.css";
+import AnimCursor from "./Components/AnimatedCursor/AnimCursor";
+import GetInTouch from "./Components/GetInTouch/GetInTouch";
+import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import Projects from "./Components/Projects/Projects";
+import Skills from "./Components/Skills/Skills";
 
 function App() {
   return (
-    <div >
-     <Navbar></Navbar>
-     <Header></Header>
-     <Skills></Skills>
-     <Projects></Projects>
-     <GetInTouch></GetInTouch>
+    <div>
+      <AnimCursor></AnimCursor>
+      <Navbar></Navbar>
+      <Header></Header>
+      <Skills></Skills>
+      <Projects></Projects>
+      <GetInTouch></GetInTouch>
     </div>
   );
 }
---
+++ src/Components/AnimatedCursor/AnimCursor.js
@@ -0,0 +1,34 @@
+import React from "react";
+import AnimatedCursor from "react-animated-cursor";
+
+const AnimCursor = () => {
+  return (
+    <div>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={12}
+        color="202, 138, 4"
+        outerAlpha={0.2}
+        hasBlendMode={true}
+        
+        innerScale={0.9}
+        outerScale={5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
+    </div>
+  );
+};
+
+export default AnimCursor;
